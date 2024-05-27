54e2164b44923f90ba566b4740a84fe44639eaa4
Petter <petterdouglas7@gmail.com>
2024-05-27 15:24:56 -0300

    criação do state para alternancia de pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
+import { useState } from 'react'
 import LandingPage from './pages/LandingPage'
 import Sobre from './pages/Sobre'
 
 function App() {
+  const [myPages, setMyPages] = useState('Inicio')
+  const alternaPages = (name) => {
+    setMyPages(name)
+  }
   return (
     <>
-      <LandingPage/>
+      {myPages === 'Inicio' && <LandingPage alterna={alternaPages}/>}
+      {myPages === 'Sobre' && <Sobre/>}
     </>
   )
 }
