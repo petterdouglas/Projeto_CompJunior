5dc1e05900473d1b4d6fbe885284d875f9090380
Petter <petterdouglas7@gmail.com>
2024-05-27 19:25:09 -0300

    problema de recarregamento de página corrigido

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import LandingPage from './pages/LandingPage'
 import Sobre from './pages/Sobre'
 
 function App() {
-  const [myPages, setMyPages] = useState('Inicio')
+  const [myPages, setMyPages] = useState(() => {
+    const savedPage = localStorage.getItem('currentPage')
+    return savedPage ? savedPage : 'Inicio'
+  })
+  useEffect(() => {
+    localStorage.setItem('currentPage', myPages);
+  }, [myPages]);
+
   const alternaPages = (name) => {
     setMyPages(name)
   }
   return (
     <>
-      {myPages === 'Inicio' && <LandingPage alterna={alternaPages}/>}
-      {myPages === 'Sobre' && <Sobre alterna={alternaPages}/>}
+      {myPages === 'Inicio' && <LandingPage alterna={alternaPages} />}
+      {myPages === 'Sobre' && <Sobre alterna={alternaPages} />}
     </>
   )
 }
